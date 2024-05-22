053dbad16470db6b9fa6397ccc3879a33f87a403
Alex Inglis <alexinglis7799@gmail.com>
2024-05-22 17:52:23 -0400

    update cors origin for netlify

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: "https://peecer.netlify.app",
     credentials: true,
   })
 );
