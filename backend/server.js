29bab79b7d29549c7ee55fcd798245a5b6e19240
Alex Inglis <alexinglis7799@gmail.com>
2024-05-25 17:50:28 -0400

    add s to http for cors

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://peecer.com",
+    origin: "https://peecer.com",
     // origin: "*",
     credentials: true,
   })
